23da63e755fa7c5937967290045bddb0d6b0daae
chanmadsen <87548465+chanmadsen@users.noreply.github.com>
2021-09-20 09:12:13 -0600

    Add files via upload

---
+++ UGolf/UGolf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UGolf
+//
+//  Created by Lon Chandler Madsen on 9/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
