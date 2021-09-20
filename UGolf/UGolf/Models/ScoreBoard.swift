764a8aa1ea4c84e0e5fbc7b722aee50a25ebbeb7
hyrum <bredisfrend@Hyrums-MacBook-Pro.local>
2021-09-20 16:11:48 -0700

    Added scoreboard model, scoreboard vc, and added some items to 'game storyboard'

---
+++ UGolf/UGolf/Models/ScoreBoard.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreBoard.swift
+//  UGolf
+//
+//  Created by Hyrum Glen Bateman on 9/20/21.
+//
+
+import Foundation
+
+class ScoreBoard {
+    let nineHole : [Int]
+    let eighteenHole : [Int]
+    var score : Int
+    let isNineHoleGame : Bool
+    
+    init(nineHole : [Int], eighteenHole : [Int], score : Int, isNineHoleGame : Bool = true)
+}
