95337b288cd7198fb6224d00acd1306a22f4e322
hyrum <bredisfrend@Hyrums-MacBook-Pro.local>
2021-09-22 11:53:50 -0700

    Rewrote the model, started the model controller

---
+++ UGolf/UGolf/Controllers/Model Controllers/ScoreBoardController.swift
@@ -0,0 +1,19 @@
+//
+//  ScoreBoardController.swift
+//  UGolf
+//
+//  Created by Hyrum Glen Bateman on 9/21/21.
+//
+
+import Foundation
+
+class ScoreBoardController {
+    let sharedInstance = ScoreBoardController()
+    
+    func parCalculator(par : Int, score : Int) {
+        guard let score = score,
+              let par = par else { return }
+        
+        
+    }
+}
---
+++ UGolf/UGolf/Controllers/View Controllers/GameViewControllers/GameSetupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameSetupViewController.swift
+//  UGolf
+//
+//  Created by Hyrum Glen Bateman on 9/21/21.
+//
+
+import UIKit
+
+class GameSetupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
