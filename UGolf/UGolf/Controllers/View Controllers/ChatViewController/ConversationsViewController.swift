c1ac12e15e0d96db477c55cefd2bc4aa31497960
Lon Madsen <braveshears@gmail.com>
2021-10-06 14:55:20 -0600

    Fixed bugs and avatar

--- UGolf/UGolf/Controllers/View Controllers/ChatViewController/ConversationsViewController.swift
+++ UGolf/UGolf/Controllers/View Controllers/ChatViewController/ConversationsViewController.swift
@@ -9,19 +9,6 @@ import UIKit
 import FirebaseAuth
 import JGProgressHUD
 
-struct Conversation {
-    let id: String
-    let name: String
-    let otherUserEmail: String
-    let latestMessage: LatestMessage
-}
-
-struct LatestMessage {
-    let date: String
-    let text: String
-    let isRead: Bool
-}
-
 class ConversationsViewController: UIViewController {
     
     private let spinner = JGProgressHUD(style: .dark)
@@ -54,32 +41,41 @@ class ConversationsViewController: UIViewController {
         view.addSubview(tableView)
         view.addSubview(noConversationsLabel)
         setupTableView()
-        fetchConversations()
         startListeningForConversations()
         
         loginObserver = NotificationCenter.default.addObserver(forName: .didLogInNotification, object: nil, queue: .main, using: {[weak self] _ in
             guard let strongSelf = self else { return }
-            strongSelf.fetchConversations()
+            
         })
     }
     
     private func startListeningForConversations() {
         guard let email = UserDefaults.standard.value(forKey: "email") as? String else { return }
+        
         if let observer = loginObserver {
             NotificationCenter.default.removeObserver(observer)
         }
+        
         let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+        
         DatabaseManager.shared.getAllConversations(for: safeEmail, completion: { [weak self] result in
             switch result {
             case .success(let conversations):
                 guard !conversations.isEmpty else {
+                    self?.tableView.isHidden = true
+                    self?.noConversationsLabel.isHidden = false
                     return
                 }
+                self?.noConversationsLabel.isHidden = true
+                self?.tableView.isHidden = false
                 self?.conversations = conversations
+                
                 DispatchQueue.main.async {
                     self?.tableView.reloadData()
                 }
             case .failure(let error):
+                self?.tableView.isHidden = true
+                self?.noConversationsLabel.isHidden = false
                 print("failed to get conversations: \(error)")
             }
             
@@ -144,6 +140,7 @@ class ConversationsViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+        noConversationsLabel.frame = CGRect(x: 10, y: (view.height-100)/2, width: view.width-20, height: 100)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -165,10 +162,7 @@ class ConversationsViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
     }
-    
-    private func fetchConversations() {
-        tableView.isHidden = false
-    }
+  
     
 
 }
